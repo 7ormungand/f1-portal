10daf897db493a2ac502de7f6bdd7bb4070eb762
7ormungand <alex.chi.07@mail.ru>
2026-07-14 17:16:53 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,21 @@
 const CACHE_NAME = 'f1-cache-v2';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/scripts.js',
-  '/manifest.json',
-  '/news/',
-  '/news/index.html',
-  '/results/',
-  '/results/index.html',
-  '/drivers/',
-  '/drivers/index.html',
-  '/reviews/',
-  '/reviews/index.html',
-  '/todolist/',
-  '/todolist/index.html'
-  '/img/Logo-F1.svg',  
+  './',                 
+  'index.html',          
+  'css/style.css',       
+  'js/scripts.js',      
+  'manifest.json',      
+  'img/Logo-F1.svg',     
+  'news/',              
+  'news/index.html',    
+  'results/',            
+  'results/index.html',  
+  'drivers/',            
+  'drivers/index.html',  
+  'reviews/',            
+  'reviews/index.html',  
+  'todolist/',           
+  'todolist/index.html' 
 ];
 
 // кэширование ресурсов
@@ -43,17 +43,20 @@ self.addEventListener('activate', (event) => {
 });
 
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      
-      return Promise.all(
-        ASSETS.map((url) => {
-          return cache.add(url).catch((err) => {
-            console.error(`Не удалось загрузить в кэш: ${url}`, err);
-          });
-        })
-      );
+self.addEventListener('fetch', (event) => {
+  let request = event.request;
+  const url = new URL(request.url);
+  if (url.origin === location.origin && !url.pathname.match(/\.[A-Za-z0-9]+$/)) 
+    if (!url.pathname.endsWith('/')) {
+      request = new Request(url.pathname + '/index.html');
+    } else {
+      request = new Request(url.pathname + 'index.html');
+    }
+  }
+
+  event.respondWith(
+    caches.match(request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
     })
   );
 });
