e066711f5c1bf1b4ec626656a13519ea5331ff7e
7ormungand <alex.chi.07@mail.ru>
2026-07-14 17:24:08 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,21 @@
-const CACHE_NAME = 'f1-cache-v4';
+const CACHE_NAME = 'f1-cache-v5';
 const ASSETS = [
-  './',                 
-  'index.html',          
-  'css/style.css',       
-  'js/scripts.js',      
-  'manifest.json',      
-  'img/Logo-F1.svg',     
-  'news/',              
-  'news/index.html',    
-  'results/',            
-  'results/index.html',  
-  'drivers/',            
-  'drivers/index.html',  
-  'reviews/',            
-  'reviews/index.html',  
-  'todolist/',           
-  'todolist/index.html' 
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/scripts.js',
+  '/manifest.json',
+  '/img/Logo-F1.svg',       
+  '/img/fia.svg',           
+  '/img/f1_tv.svg',
+  '/news/index.html',       
+  '/results/index.html',   
+  '/drivers/index.html',   
+  '/reviews/index.html',    
+  '/todolist/index.html'
 ];
 
-// кэширование ресурсов
+// кэширование
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -27,7 +24,7 @@ self.addEventListener('install', (event) => {
   );
 });
 
-//  удаление старого кэша
+// удаление старого кэша
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
@@ -42,20 +39,9 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-
 self.addEventListener('fetch', (event) => {
-  let request = event.request;
-  const url = new URL(request.url);
-  if (url.origin === location.origin && !url.pathname.match(/\.[A-Za-z0-9]+$/)) 
-    if (!url.pathname.endsWith('/')) {
-      request = new Request(url.pathname + '/index.html');
-    } else {
-      request = new Request(url.pathname + 'index.html');
-    }
-  }
-
   event.respondWith(
-    caches.match(request).then((cachedResponse) => {
+    caches.match(event.request).then((cachedResponse) => {
       return cachedResponse || fetch(event.request);
     })
   );
